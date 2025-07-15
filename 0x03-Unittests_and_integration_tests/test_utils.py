b239d4619ae74b9417b12f80100b4803df4f92eb
Enoch Baah <ebaah2942@gmail.com>
2025-07-15 23:26:40 +0000

    Added new checks

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,36 +5,35 @@ from utils import access_nested_map
 from unittest.mock import patch, Mock
 from utils import get_json
 
-# class TestAccessNestedMap(unittest.TestCase):
-#     @parameterized.expand([
-#         ({"a": 1}, ("a",), 1),
-#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-#         ({"a": {"b": 2}}, ("a", "b"), 2),
-#     ])
-#     def test_access_nested_map(self, nested_map, path, expected):
-#         self.assertEqual(access_nested_map(nested_map, path), expected)
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
  
 
 
-#     @parameterized.expand([
-#         ({}, ("a",), 'a'),
-#         ({"a": 1}, ("a", "b"), 'b'),
-#     ])
-#     def test_access_nested_map_exception(self, nested_map, path, expected_key):
-#         with self.assertRaises(KeyError) as context:
-#             access_nested_map(nested_map, path)
-#         self.assertEqual(str(context.exception), f"'{expected_key}'")
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b'),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_key}'")
 
 
     
 
 class TestGetJson(unittest.TestCase):
-
     @parameterized.expand([
-        ("example_com", "http://example.com", {"payload": True}),
-        ("holberton_io", "http://holberton.io", {"payload": False}),
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(self, name, test_url, test_payload):
+    def test_get_json(self, test_url, test_payload):
         mock_response = Mock()
         mock_response.json.return_value = test_payload
 
