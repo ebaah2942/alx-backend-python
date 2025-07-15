417deb4a29977a5650bcef0f9580e9c49035c977
Enoch Baah <ebaah2942@gmail.com>
2025-07-15 22:29:59 +0000

    Fixed some bugs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,27 +13,21 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+ 
 
-    # @parameterized.expand([
-    #     ({}, ("a",), 'a'),
-    #     ({"a": 1}, ("a", "b"), 'b'),
-    # ])
-    # def test_access_nested_map_exception(self, nested_map, path, expected_key):
-    #     with self.assertRaises(KeyError) as context:
-    #         access_nested_map(nested_map, path)
-
-    #     self.assertEqual(str(context.exception, f"'{expected_key}'"))   
 
     @parameterized.expand([
-    ({}, ("a",), KeyError, 'a'),
-    ({"a": 1}, ("a", "b"), TypeError, None),
-])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception, expected_key):
-        with self.assertRaises(expected_exception) as context:
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b'),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
 
-        if expected_key:
-            self.assertEqual(str(context.exception), f"'{expected_key}'")
+        self.assertEqual(str(context.exception), f"'{expected_key}'")
+
+        
+                
 
 
  
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,5 +1,8 @@
 def access_nested_map(nested_map, path):
     for key in path:
-        nested_map = nested_map[key]
+        try:
+            nested_map = nested_map[key]
+        except (KeyError, TypeError):
+            raise KeyError(key)
     return nested_map
 
