6b8647e48a051adb19f2d0fab5906b1025361801
Enoch Baah <ebaah2942@gmail.com>
2025-07-30 01:22:04 +0000

    Added new chnages to the unread managers

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -36,9 +36,9 @@ def conversation_view(request):
 
     return render(request, 'messaging/conversation.html', {'messages': messages})
 
+
 @login_required
 def unread_messages_view(request):
-    unread_messages = Message.unread.unread_for_user(request.user)
+    unread_messages = Message.unread.unread_for_user(request.user).only('id', 'sender', 'content', 'timestamp')
     return render(request, 'messaging/unread_messages.html', {'unread_messages': unread_messages})
-
 
