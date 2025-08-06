1f89c1e0a934999d34f0b46458adc6f9a29b12c0
Enoch Baah <ebaah2942@gmail.com>
2025-08-06 01:41:16 +0000

    Added new stuffs

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'NAME': os.environ.get('MYSQL_DB'),
         'USER': os.environ.get('MYSQL_USER'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
